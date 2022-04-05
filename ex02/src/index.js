f90f1f48984e94db15e7e13a248406c4e08d7a3b
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-05 11:38:39 +0200

    Add Exercise 02

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function myFunction(myParameter){
+  if(myParameter){
+      return "The parameter is true!";
+  }  
+  return "The parameter is false!";
+}
+
+myFunction(true);
+myFunction(false);
+console.log(myFunction(true));
+console.log(myFunction(false));
+module.exports = myFunction;
