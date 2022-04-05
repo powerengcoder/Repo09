b2f884bb895e99503895dd5351c6f0a4b582503f
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-05 21:20:32 +0200

    Update Exercise 11

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -5,7 +5,11 @@ if(num <= 5){
     return "Bigger than 5"
 }
 }
+functionElse(4);
 functionElse(5);
+functionElse(6);
+functionElse(10);
+
 console.log(functionElse(4));
 console.log(functionElse(5));
 console.log(functionElse(6));
