f755f7859447f71190375f5846f5f880623ac93d
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-06 00:27:28 +0200

    Add Exercise 19

---
+++ ex19/src/index.js
@@ -0,0 +1,21 @@
+function myDoWhile(){
+    var i = 0;
+    var myNumbers ="";
+
+    do{
+        if (i == 9){
+            myNumbers += i;
+        } else {
+        myNumbers += i + ", ";
+        }
+
+        i++;
+
+    } while(i<10);
+
+return myNumbers;
+}
+
+myDoWhile();
+console.log(myDoWhile());
+module.exports = myDoWhile;
