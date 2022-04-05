5ddb7ee4bf883057efeb03d21dee1cca78735945
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-06 00:15:43 +0200

    Add Exercise 18

---
+++ ex18/src/index.js
@@ -0,0 +1,33 @@
+function myForLoop1(){
+var evenNumbers ="";
+for (var i = 0; i < 10; i++){
+if (i == 8 && i%2 == 0){
+        evenNumbers += i;
+    } else if (i%2 == 0 ){
+        evenNumbers += i + ", ";
+    }
+}
+return evenNumbers;
+}
+
+
+function myForLoop2(){
+    var evenInverseNumbers="";
+for (var i= 8; i >= 0; i--){
+    if(i == 0 && i%2 == 0){
+        evenInverseNumbers +=i;
+    }else if (i%2 == 0){
+            evenInverseNumbers += i + ", ";
+        }
+    }
+return evenInverseNumbers; 
+}
+myForLoop1();
+myForLoop2();
+console.log(myForLoop1());
+console.log(myForLoop2());
+
+module.exports = {
+    myForLoop1,
+    myForLoop2
+}
